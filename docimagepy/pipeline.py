fbbe48c5bb113ce5f881aeefa7f85c523e18ee66
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2022-07-22 11:56:35 +0530

    Create pipeline.py

---
+++ docimagepy/pipeline.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from sklearn import preprocessing
+import joblib
+import re
+import pytesseract
+
+class DocImage:
+  
