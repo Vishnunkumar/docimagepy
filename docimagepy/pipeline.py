f543fa72c6457a2cbebf6f92c970b01a0a3d8b08
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2022-07-27 11:33:51 +0530

    updated code for angle finder

--- docimagepy/pipeline.py
+++ docimagepy/pipeline.py
@@ -10,40 +10,42 @@ import re
 import pytesseract
 
 class DocImage:
-"""
-The above code is loading the model and scaler and then using the model to 
-predict the angle of rotation of the image.
-"""
-  def __init__(self, scaler, model):
-    self.scaler = scaler
-    self.model = model
-
+  """
+  The above code is loading the model and scaler and then using the model to 
+  predict the angle of rotation of the image.
+  """
+  def __init__(self, imge):
+    self.imge = imge
   
   def load_models(self):
-  """
-  Loading the model and scaler.
-  """  
+    """
+    Loading the model and scaler.
+    """  
+    
     self.model = tf.keras.models.load_model('src/model.h5')
     self.scaler = joblib.load('src/sscaler.gz') 
 
- 
-  def get_rotate_angle(self, scaler, model, imge):
-  """
-  The above code is used to rotate the image to the correct angle.
-  """
+    return self.model, self.scaler
+
+  def get_rotate_angle(self, scaler, model, imge, th_angle):
+
+    """
+    The above code is used to rotate the image to the correct angle.
+    """
+    self.th_angle = th_angle
     img_width, img_height = 224, 224    
     rt_angle = 10
     f_angle = 0
-    while abs(rt_angle) > 8:
-        imgex = cv2.resize(imge, (img_width, img_height))
+    while abs(rt_angle) > self.th_angle:
+        imgex = cv2.resize(self.imge, (img_width, img_height))
         imgex = imgex.reshape(1, img_height, img_width, 3)
         imgex = imgex/255
-        angle = loaded_model.predict(imgex)
-        rt_angle = scaler.inverse_transform(angle)[0][0]
+        angle = self.model.predict(imgex)
+        rt_angle = self.scaler.inverse_transform(angle)[0][0]
         rot_angle = -1*(rt_angle)
-        im1 = Image.fromarray(imge)
+        im1 = Image.fromarray(self.imge)
         im2 = im1.rotate(rot_angle, PIL.Image.NEAREST, expand = 0, fillcolor = 'white')
-        imge = np.array(im2)
+        self.imge = np.array(im2)
         f_angle = f_angle + rot_angle
 
     return f_angle
